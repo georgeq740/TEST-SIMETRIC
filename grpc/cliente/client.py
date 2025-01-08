c9f10f4b347620d326111bac493bd219564708b3
georgeq740 <jorge.quitian740@gmail.com>
2025-01-08 03:10:20 -0500

    Update client.py

--- grpc/cliente/client.py
+++ grpc/cliente/client.py
@@ -3,7 +3,7 @@ import example_pb2
 import example_pb2_grpc
 
 def run():
-    with grpc.insecure_channel("app2:50051") as channel:
+    with grpc.insecure_channel("servidor-service.default.svc.cluster.local:50051") as channel:
         stub = example_pb2_grpc.ExampleServiceStub(channel)
         response = stub.SayHello(example_pb2.HelloRequest(name="World"))
         print(f"Response from server: {response.message}")
