5ad29234969f3e5f4f28e620290cf2bade0bb564
georgeq740 <jorge.quitian740@gmail.com>
2025-01-08 03:04:20 -0500

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -22,3 +22,20 @@ variable "aws_region" {
   description = "Región de AWS donde se desplegará el clúster"
   type        = string
 }
+
+variable "cliente_image" {
+  description = "URL de la imagen Docker del cliente en Amazon ECR"
+  type        = string
+}
+
+variable "servidor_image" {
+  description = "URL de la imagen Docker del servidor en Amazon ECR"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Puerto que expone el contenedor"
+  type        = number
+  default     = 50051
+}
+
