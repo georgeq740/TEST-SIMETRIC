f12c667c4aecb527d3dcf207502be3b9b20917cd
georgeq740 <jorge.quitian740@gmail.com>
2025-01-07 21:42:12 -0500

    Delete IAC directory

--- IAC/app/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "s3-terraform-state-jorge-quitian"
-    key            = "simetric/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = "true"
-  }
-}
