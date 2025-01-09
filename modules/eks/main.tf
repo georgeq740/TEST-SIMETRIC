5810d66115eef766c94d997a291b87ed8d4bfaf3
Jorge Luis Quitian <george_740@hotmail.com>
2025-01-08 21:49:54 -0500

    test

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,12 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = concat(var.public_subnet_ids, var.private_subnet_ids)
+  }
+}
+
 # Rol y políticas para el AWS Load Balancer Controller
 resource "aws_iam_role" "alb_controller_role" {
   name = "${var.cluster_name}-alb-controller-role"
