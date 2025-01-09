136107bc284fe8f5de61a481754ddbd640053982
Jorge Luis Quitian <george_740@hotmail.com>
2025-01-08 21:40:54 -0500

    update

--- app/main.tf
+++ app/main.tf
@@ -22,7 +22,7 @@ module "vpc" {
 module "eks" {
   source             = "../modules/eks"         
   cluster_name       = "test-simetric"         
-  aws_region         = module.config.region            
+  region         = module.config.region            
   public_subnet_ids  = module.vpc.public_subnet_ids  
   private_subnet_ids = module.vpc.private_subnet_ids 
   vpc_id             = module.vpc.vpc_id       
